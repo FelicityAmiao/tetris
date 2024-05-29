cd6626afd7c7e4a5cc635f294e28d24d4f9f3e39
felicityamiao <1760077814@qq.com>
2024-05-29 21:52:55 +0800

    F4. detect the containerPanel location.

--- src/main/java/com/felygame/tetris/Game.java
+++ src/main/java/com/felygame/tetris/Game.java
@@ -4,6 +4,8 @@ import com.felygame.tetris.panel.MainPanel;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ComponentAdapter;
+import java.awt.event.ComponentEvent;
 
 public class Game {
 
--- src/main/java/com/felygame/tetris/panel/ContainerPanel.java
+++ src/main/java/com/felygame/tetris/panel/ContainerPanel.java
@@ -3,9 +3,28 @@ package com.felygame.tetris.panel;
 import com.felygame.tetris.block.GridBlock;
 
 import javax.swing.*;
+import javax.swing.event.AncestorEvent;
+import javax.swing.event.AncestorListener;
+import java.awt.*;
 
 public class ContainerPanel {
   public static JPanel create() {
-    return GridBlock.create(1, 1);
+    JPanel container = GridBlock.create(1, 1);
+    container.addAncestorListener(new AncestorListener() {
+      @Override
+      public void ancestorAdded(AncestorEvent event) {
+        Point containerLocation = container.getLocation();
+        System.out.println("Container location in parent panel: " + containerLocation);
+      }
+
+      @Override
+      public void ancestorRemoved(AncestorEvent event) {
+      }
+
+      @Override
+      public void ancestorMoved(AncestorEvent event) {
+      }
+    });
+    return container;
   }
 }
