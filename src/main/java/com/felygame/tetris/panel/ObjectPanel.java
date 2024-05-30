54b70a8ec27cd9540c843ad42cf8d1bffdebbe20
felicityamiao <1760077814@qq.com>
2024-05-30 23:49:25 +0800

    R2.F2. adjust objectPanel init position to fill container.

--- src/main/java/com/felygame/tetris/panel/ObjectPanel.java
+++ src/main/java/com/felygame/tetris/panel/ObjectPanel.java
@@ -33,9 +33,17 @@ public class ObjectPanel {
   });
 
   public static JPanel create() {
-    JPanel objectPanel = Block.create(LIGHT_DARK_ORIGIN);
+    JPanel objectPanel = new JPanel();
+    objectPanel.setLayout(new BoxLayout(objectPanel, BoxLayout.X_AXIS));
+    objectPanel.add(getObject());
+    objectPanel.add(Box.createRigidArea(new Dimension(Block.WIDTH,  Block.HEIGHT)));
+    objectPanel.setOpaque(false);
     KeyEventHelper.bindKeyEvent(objectPanel, KeyEventHelper.KEY_DOWN, Y_SPEED, detectReachContainer);
     return objectPanel;
+  }
+
+  private static JPanel getObject() {
+    return Block.create(LIGHT_DARK_ORIGIN);
   }
 
 }
