6536c8954b2a19b0b1aa95127f70bb24040f781b
felicityamiao <1760077814@qq.com>
2024-05-31 00:01:13 +0800

    R2.F3. add detectReachContainer callback for both left or right

--- src/main/java/com/felygame/tetris/panel/ObjectPanel.java
+++ src/main/java/com/felygame/tetris/panel/ObjectPanel.java
@@ -37,7 +37,7 @@ public class ObjectPanel {
   public static JPanel create() {
     JPanel objectPanel = new JPanel();
     objectPanel.setLayout(new BoxLayout(objectPanel, BoxLayout.X_AXIS));
-    objectPanel.add(getObject());
+    objectPanel.add(Block.create(LIGHT_DARK_ORIGIN));
     objectPanel.add(Box.createRigidArea(new Dimension(Block.WIDTH,  Block.HEIGHT)));
     objectPanel.setOpaque(false);
     bindKeyEvents(objectPanel);
@@ -46,12 +46,8 @@ public class ObjectPanel {
 
   private static void bindKeyEvents(JPanel objectPanel) {
     KeyEventHelper.bindKeyEvent(objectPanel, KeyEventHelper.KEY_DOWN, Y_SPEED, detectReachContainer);
-    KeyEventHelper.bindKeyEvent(objectPanel, KeyEventHelper.KEY_LEFT, X_SPEED, null);
-    KeyEventHelper.bindKeyEvent(objectPanel, KeyEventHelper.KEY_RIGHT, X_SPEED, null);
-  }
-
-  private static JPanel getObject() {
-    return Block.create(LIGHT_DARK_ORIGIN);
+    KeyEventHelper.bindKeyEvent(objectPanel, KeyEventHelper.KEY_LEFT, X_SPEED, detectReachContainer);
+    KeyEventHelper.bindKeyEvent(objectPanel, KeyEventHelper.KEY_RIGHT, X_SPEED, detectReachContainer);
   }
 
 }
