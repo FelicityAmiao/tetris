34c55236dc16ff57271828f9295c5096b9d5f280
felicityamiao <1760077814@qq.com>
2024-06-03 19:09:58 +0800

    R2.F4. sticky - refactor: extract detectHelper.

--- src/main/java/com/felygame/tetris/panel/ObjectPanel.java
+++ src/main/java/com/felygame/tetris/panel/ObjectPanel.java
@@ -5,45 +5,15 @@ import com.felygame.tetris.util.KeyEventHelper;
 
 import javax.swing.*;
 import java.awt.*;
-import java.util.function.Consumer;
 
 import static com.felygame.tetris.enums.Color.LIGHT_DARK_ORIGIN;
+import static com.felygame.tetris.util.DetectHelper.detects;
 
 public class ObjectPanel {
 
   public static final int TOTAL_HEIGHT = 501;
   public static final int Y_SPEED = 30;
   private static final int X_SPEED = 30;
-
-  private static final Consumer<JPanel> detectReachContainer = (jpanel -> {
-    Point objectLocation = jpanel.getLocation();
-    if (objectLocation.getY() + jpanel.getHeight() < TOTAL_HEIGHT) {
-      return;
-    }
-    SwingUtilities.invokeLater(() -> {
-      JOptionPane.showMessageDialog(null, "You win!", "Congratulations", JOptionPane.INFORMATION_MESSAGE);
-      System.exit(0);
-    });
-  });
-  private static Consumer<JPanel> detectSticky = (jpanel -> {
-    Point objectLocation = jpanel.getLocation();
-    if (objectLocation.getY() + jpanel.getHeight() < TOTAL_HEIGHT) {
-      return;
-    }
-    sticky(jpanel);
-  });
-
-  private static void sticky(JPanel jpanel) {
-    KeyEventHelper.unbindKeyEvent(jpanel, KeyEventHelper.KEY_DOWN);
-    KeyEventHelper.unbindKeyEvent(jpanel, KeyEventHelper.KEY_LEFT);
-    KeyEventHelper.unbindKeyEvent(jpanel, KeyEventHelper.KEY_RIGHT);
-    KeyEventHelper.unbindKeyEvent(jpanel, KeyEventHelper.KEY_SPACE);
-  }
-
-  private static Consumer[] detects = new Consumer[]{
-      detectReachContainer,
-      detectSticky
-  };
 
   public static JPanel create() {
     JPanel objectPanel = new JPanel();
---
+++ src/main/java/com/felygame/tetris/util/DetectHelper.java
@@ -0,0 +1,47 @@
+package com.felygame.tetris.util;
+
+import javax.swing.*;
+import java.util.function.Consumer;
+
+import static com.felygame.tetris.panel.ObjectPanel.TOTAL_HEIGHT;
+
+public class DetectHelper {
+
+  private static final Consumer<JPanel> detectReachContainer = (jpanel -> {
+    if (notReachMainPanelBottom(jpanel)) {
+      return;
+    }
+    gameOver();
+  });
+
+  private static Consumer<JPanel> detectSticky = (jpanel -> {
+    if (notReachMainPanelBottom(jpanel)) {
+      return;
+    }
+    sticky(jpanel);
+  });
+
+  public static Consumer[] detects = new Consumer[]{
+      detectReachContainer,
+      detectSticky
+  };
+
+  private static void gameOver() {
+    SwingUtilities.invokeLater(() -> {
+      JOptionPane.showMessageDialog(null, "You win!", "Congratulations", JOptionPane.INFORMATION_MESSAGE);
+      System.exit(0);
+    });
+  }
+
+  private static boolean notReachMainPanelBottom(JPanel jpanel) {
+    return jpanel.getLocation().getY() + jpanel.getHeight() < TOTAL_HEIGHT;
+  }
+
+  private static void sticky(JPanel jpanel) {
+    KeyEventHelper.unbindKeyEvent(jpanel, KeyEventHelper.KEY_DOWN);
+    KeyEventHelper.unbindKeyEvent(jpanel, KeyEventHelper.KEY_LEFT);
+    KeyEventHelper.unbindKeyEvent(jpanel, KeyEventHelper.KEY_RIGHT);
+    KeyEventHelper.unbindKeyEvent(jpanel, KeyEventHelper.KEY_SPACE);
+  }
+
+}
