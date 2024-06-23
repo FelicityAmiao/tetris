03818a2cc2185ac1acf6728a631f6c750da19783
FelicityAmiao <1760077814@qq.com>
2024-06-24 00:01:05 +0800

    [Tetris-OneBlock-0.1] F1. move down.

---
+++ js/constants/common.js
@@ -0,0 +1,6 @@
+window.KEY_UP = 'ArrowUp';
+window.KEY_DOWN = { key: 'ArrowDown', direction: 'marginTop' };
+window.KEY_LEFT = 'ArrowLeft';
+window.KEY_RIGHT = 'ArrowRight';
+
+window.STEP = 100;
--- js/game.js
+++ js/game.js
@@ -1,3 +1,10 @@
 document.addEventListener("DOMContentLoaded", () => {
-  alert('hi');
+  let block = document.querySelector('.block');
+  block.focus();
+  block.addEventListener("keydown", (event) => {
+    if (event.key === window.KEY_DOWN.key) {
+      let currentMarginTop = parseInt(block.style[window.KEY_DOWN.direction]) || 0;
+      block.style[window.KEY_DOWN.direction] = currentMarginTop + window.STEP + 'px';
+    }
+  });
 });
