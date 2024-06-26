111072526193eb3a421a616ea9f16ba413446ebe
FelicityAmiao <1760077814@qq.com>
2024-06-26 21:48:06 +0800

    [Tetris-OneBlock-0.1] F1. land

--- js/constants/common.js
+++ js/constants/common.js
@@ -2,3 +2,5 @@ window.KEY_UP = { key: 'ArrowUp', direction: 'marginBottom', step: 100 };
 window.KEY_DOWN = { key: 'ArrowDown', direction: 'marginTop', step: 100 };
 window.KEY_LEFT = 'ArrowLeft';
 window.KEY_RIGHT = 'ArrowRight';
+
+window.KEY_SPACE = { key: ' ', direction: 'marginTop', totalStep: 1000 };
--- js/game.js
+++ js/game.js
@@ -2,8 +2,16 @@ function move(block, action) {
   if (isLand(block)) {
     return;
   }
-  let currentLocation = parseInt(block.style[action.direction]) || 0;
-  block.style[action.direction] = currentLocation + action.step + 'px';
+  let currentMove = parseInt(block.style[action.direction]) || 0;
+  block.style[action.direction] = currentMove + action.step + 'px';
+}
+
+function land(block) {
+  if (isLand(block)) {
+    return;
+  }
+  let action = window.KEY_SPACE;
+  block.style[action.direction] = action.totalStep + 'px';
 }
 
 function isLand(block) {
@@ -22,6 +30,10 @@ document.addEventListener("DOMContentLoaded", () => {
       if (isLand(block)) {
         block.blur();
       }
+    }
+    if (event.key === window.KEY_SPACE.key) {
+      land(block);
+      block.blur();
     }
   });
   setScheduler(
